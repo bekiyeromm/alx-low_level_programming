a1e6c8b1a595d2f16d10fc0bd63d7febd48ea058
bekiyeromm <bk3tena@gmail.com>
2022-09-09 11:35:30 -0700

    negative posetive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-/*more headers goes there*/
-
 /**
  * main function -entry goes there
+ *
+ * Return always 0 (success)
  */
 int main(void)
 {
