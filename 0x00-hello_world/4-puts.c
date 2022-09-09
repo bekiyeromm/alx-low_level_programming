ae3e1745772178f0845ff480649aa5d499743efd
bekiyeromm <bk3tena@gmail.com>
2022-09-09 01:33:33 -0700

    hello puts again agin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /**
- *main function entry
- *
- * return always 0
+ *main -function entry
+ *Return: always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
