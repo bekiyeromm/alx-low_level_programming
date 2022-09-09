dafbe5106324026bcbead047da92640e1b571173
bekiyeromm <bk3tena@gmail.com>
2022-09-09 16:11:18 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *main -main entry
+ *Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	for ( i= 'A'; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
