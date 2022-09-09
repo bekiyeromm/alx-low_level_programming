9dce69a439cde12e9aa70129303ae4502d23f7b9
bekiyeromm <bk3tena@gmail.com>
2022-09-09 01:27:45 -0700

    hello puts again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ *main function entry
+ *
+ * return always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
