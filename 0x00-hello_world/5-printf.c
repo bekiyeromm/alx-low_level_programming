56e5b8e0543060494827ca02f0d29b603c0f421e
bekiyeromm <bk3tena@gmail.com>
2022-09-09 01:48:28 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main -entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
