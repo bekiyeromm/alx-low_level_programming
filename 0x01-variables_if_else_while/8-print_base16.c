f3703a7132bbc39d468b1a9e2e0359331df13297
bekiyeromm <bk3tena@gmail.com>
2022-09-09 17:35:40 -0700

    hexa decimal digits

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,12 +3,12 @@
  *main -entry
  *Return: always 0
  */
-int main()
+int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <=9 ; i++)
-		putchar(i+'0');
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
 	for (i = 'a' ; i <= 'f' ; i++)
 		putchar(i);
 	putchar('\n');
