6b94047b1b6b39338e95dc309df5b63ee3a21763
bekiyeromm <bk3tena@gmail.com>
2022-09-09 19:09:45 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ *main -entry
+ *Return: always true
+ */
+int main(void)
+{
+	int i,j,n=1;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = n ; j <= 9 ; j++)
+		{
+			putchar(i + '0');
+		       	putchar(j + '0');
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+			
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
