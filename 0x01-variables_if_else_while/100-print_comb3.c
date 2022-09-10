af42d84c7980ebe999f0ad39c869036a2306be50
bekiyeromm <bk3tena@gmail.com>
2022-09-09 19:14:10 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,16 @@
 int main(void)
 {
 	int i,j,n=1;
-
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = n ; j <= 9 ; j++)
 		{
 			putchar(i + '0');
-		       	putchar(j + '0');
+			putchar(j + '0');
 			if (i == 8 && j == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
-			
 		}
 		n++;
 	}
