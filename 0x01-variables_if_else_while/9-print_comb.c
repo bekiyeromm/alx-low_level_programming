909d5c35ff7807ae59bf323347469f076f68a947
bekiyeromm <bk3tena@gmail.com>
2022-09-09 18:20:25 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 1 ; i <= 9 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
 		putchar(i + '0');
-		if(i == 9)
+		if (i == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
