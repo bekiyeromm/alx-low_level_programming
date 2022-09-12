89f6d69c484f68bff4df73dd4d0789ed92904b2b
bekiyeromm <bk3tena@gmail.com>
2022-09-12 02:46:43 -0700

    beki

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -29,8 +29,8 @@ int main(void)
 						putchar(' ');
 						putchar(e);
 						putchar(i);
-		  				if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
+						if (h == 57 && g == 56 && e == 57 && i == 57)
+							break;
 						putchar(',');
 						putchar(' ');
 					}
