9d28631b75e36d0edbe1a497085a0c9a0f563fa1
bekiyeromm <bk3tena@gmail.com>
2022-09-11 19:43:36 -0700

    number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,15 @@ int main(void)
 
 	for (i = 0 ; i <= 98 ; i++)
 	{
-		for (j = i+1 ; j <= 99 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
-			putchar((i%10) + '0');
-			putchar((i%10) + '0');
+			putchar((i % 10) + '0');
+			putchar((i % 10) + '0');
 
 			putchar(' ');
 
-			putchar((i%10) + '0');
-			putchar((j%10) + '0');
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 
 			if (i == 98 && j == 99)
 				continue;
