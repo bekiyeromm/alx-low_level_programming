01c6bda306c27be7f5456cdf158dcc749354fe51
bekiyeromm <bk3tena@gmail.com>
2022-09-11 22:52:33 -0700

    rogram that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,15 +9,15 @@ int main(void)
 
 	for (i = 0 ; i <= 7 ; i++)
 	{
-		for (j = 1 ; j <= 8 ; j++)
+		for (j = i+1 ; j <= 8 ; j++)
 		{
-			for (n = 2 ; n <= 9 ; n++)
+			for (n = j+1 ; n <= 9 ; n++)
 			{
-				if (i == n || j == n || j == i)
-					continue;
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(n + '0');
+				/**if (i == n || j == n || j == i)
+				*	continue;*/
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((n % 10) + '0');
 				if (i == 7 && j == 8 && n == 9)
 					continue;
 				putchar(',');
