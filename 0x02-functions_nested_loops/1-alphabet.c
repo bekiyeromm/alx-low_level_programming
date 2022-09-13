ef603679508b988087c352345ed18ae3a882e17a
bekiyeromm <bk3tena@gmail.com>
2022-09-13 13:16:32 -0700

    printing letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *main entry
+ *
+ *return www
+ */
+void print_alphabet(void)
+{
+	int n;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 void _putchar(char c);
+void print_alphabet(void);
 #endif
 
