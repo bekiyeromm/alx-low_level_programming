20171eab57d8676c133cd39a8f64933cc963d102
bekiyeromm <bk3tena@gmail.com>
2022-09-14 05:35:26 -0700

    adding two number

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include "main.h"
 /**
- *add -calculate sum
- *
- *Return: always 0
+ * add -calculate sum
+ * @num1: num1 to be added
+ * @num2: num2 to be added
+ * Return: always 0
  */
 int add(int num1, int num2)
 {
 	int sum;
 
-	sum=num1+num2;
+	sum = num1 + num2;
 	return (sum);
 }
