0943e6bc6ff95f3fe9122f6b1b8c97a0a8ec93a5
bekiyeromm <bk3tena@gmail.com>
2022-09-13 20:45:26 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, c;
+
+	for(i = 1; i <= 10; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
