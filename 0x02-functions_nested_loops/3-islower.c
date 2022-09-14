480bf7627be32da7eaa509e69179a3faa5962231
bekiyeromm <bk3tena@gmail.com>
2022-09-13 20:59:37 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _islower -print lowercase char
+ * @c -param
+ * Description: print lower
+ * return: always 1
+ */
+int _islower(int c)
+{
+	int c;
+	if(c >= 'a' && c <= 'z'
+		return (1);
+	else 
+		return(0);
+}
