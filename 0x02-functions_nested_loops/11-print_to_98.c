0e58609371294b039739b571a801880790a39fea
bekiyeromm <bk3tena@gmail.com>
2022-09-14 07:32:35 -0700

    number 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,19 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-	{
-		printf("98");
-
-	}
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	while (n >= 98)
+	while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
+	if (n == 98)
+	{
+		printf("%d, ", n);
+	}
 	printf("\n");
 }
