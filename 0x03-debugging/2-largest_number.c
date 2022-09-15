fdb6fdf9bc44783fe7eadc7bd9d3d0d8211f4b5b
bekiyeromm <bk3tena@gmail.com>
2022-09-15 13:01:46 -0700

    maximum of three number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+/*
+ * largest_number -find max
+ * @: param 1
+ * @b: param2
+ * @c: param3
+ * Return: always 0
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest =c;
+	}
+	return (largest);
+}
