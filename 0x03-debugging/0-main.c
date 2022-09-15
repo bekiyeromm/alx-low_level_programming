59ead3cc60d49013b914e410ef19331228b40608
bekiyeromm <bk3tena@gmail.com>
2022-09-15 12:14:17 -0700

    test that the function positive_or_negative() gives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,9 +7,9 @@
 void positive_or_negative(int i)
 {
 	if (n > 0)
-		printf("%d:is posetive",i);
+		printf("%d is positive", i);
 	else if (n < 0)
-		printf("%d: is negative",i);
+		printf("%d is negative", i);
 	else
-		printf("%d: is zero",i);
+		printf("%d is zero", i);
 }
