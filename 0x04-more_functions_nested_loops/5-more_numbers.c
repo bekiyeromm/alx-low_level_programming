e9667769833d3131763d9a980d7dcd63f1080be9
bekiyeromm <bk3tena@gmail.com>
2022-09-15 07:58:22 -0700

    Write a function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			_putchar((j / 10) + '0');
 		}
-		_putchar('\n');
+		_putchar((j % 10) + '0');
 	}
 	_putchar('\n');
 }
