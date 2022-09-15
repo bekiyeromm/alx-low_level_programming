aaca2257e424833723d4cf1382eb50189b857b31
bekiyeromm <bk3tena@gmail.com>
2022-09-14 19:34:07 -0700

    sum of factor

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main -main entry
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int i, j, a, b, sum = 0;
+
+	 for (i = 0 ; i < = (1024 / 3); i++)
+	 {
+		 a = 3 * i;
+		 sum = sum + a;
+	 }
+	 for (j = 0 ; j <= (1024 / 5); j++)
+	 {
+		 if (!(f % 3 == 0))
+		 {
+			 b = 5 * j;
+			 sum = sum + j;
+		 }
+	 }
+	 printf("%d\n", s);
+	 return (0);
+}
