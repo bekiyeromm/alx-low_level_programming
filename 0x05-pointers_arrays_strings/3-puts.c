34baa21630be3a1860ca7866a96fee8309b70d80
bekiyeromm <bk3tena@gmail.com>
2022-09-19 09:23:37 -0700

    function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,12 @@
  */
 void _puts(char *str)
 {
-	char st[100];
+	int i = 0;
 
-	str = st;
-	while (*str != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
