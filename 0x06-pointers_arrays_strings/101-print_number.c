c6064f50a26d304df1e3666d92557b3d24f8d489
bekiyeromm <bk3tena@gmail.com>
2022-09-22 13:03:44 -0700

    Write a function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_number -function
+ *
+ * @n: parameter
+ *
+ *
+ */
+void print_number(int n)
+{
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
+
+	}
+}
