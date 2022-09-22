015ade28c2e11811f3914a6fcd1d2bfd80ee83d3
bekiyeromm <bk3tena@gmail.com>
2022-09-21 22:46:27 -0700

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 	if (str[i] >= 'a' && str[i] <= 'z')
 		str[i] = str[i] - 32;
 	i++;
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i] == ' ' || str[i] == ',' ||
 str[i] == '"' || str[i] == '{'
