536af4877139a7f6c1dbd33783dc8c57c43de237
bekiyeromm <bk3tena@gmail.com>
2022-09-22 01:54:35 -0700

    readme file modified

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include "main.h"
+/**
+ * _strcat -concatenate two string
+ *
+ * @dest: destination string
+ * @src: source string
+ * Return: returns destinatin string
+ * @n: number of byte taken from src
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
+}
