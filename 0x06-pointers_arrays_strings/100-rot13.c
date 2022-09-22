07c7baaf34f742d12a2aae60d02fb1e86a8f2390
bekiyeromm <bk3tena@gmail.com>
2022-09-22 13:02:07 -0700

    Write a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * rot13 -function
+ * @str: parameter
+ * Return: returns the string
+ *
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
