f180b468749b840dbdb9ff0127ab35486337a9f2
bekiyeromm <bk3tena@gmail.com>
2022-09-21 23:52:05 -0700

    a function that encodes a string into 1337.Letters a and A should be replaced by 4.....

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include<stdion.h>
+#include "main.h"
+/**
+ * leet -fuction
+ *
+ * @str: string
+ * Return: return string str
+ */
+char *leet(char *str)
+{
+	char str1[20] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	char str2[20] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; str1[j] != '\0'; i++)
+		{
+			if (str[i] == str1[i])
+				str[i] = str2[j];
+		}
+	}
+	return (str);
+}
