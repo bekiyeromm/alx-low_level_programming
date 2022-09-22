4ac2d2be63cec592c49a687cb9af8d0f5447403e
bekiyeromm <bk3tena@gmail.com>
2022-09-21 22:43:53 -0700

    a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,20 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
+	i++;
+	while (s[i] != '\0')
 	{
-		if (i == 0)
-		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32;
-			continue;
-		}
-		if (str[i] == ' ' || str[i] == ',' || str[i] == '"' || str[i] == '{'
-|| str[i] == '\n' || str[i] ==' '
-|| str[i] == '\t'
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i] == ' ' || str[i] == ',' ||
+str[i] == '"' || str[i] == '{'
+|| str[i] == '\n' || str[i] == ' ' || str[i] == '\t'
 || str[i] == '}' || str[i] == ')' || str[i] == '.' || str[i] == '(' ||
-str[i] == '?' || str[i] == '!' || str[i] == ';')
-		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+str[i] == '?' || str[i] == '!' || str[i] == ';'))
+			str[i] = str[i] - 32;
+			i++;
 	}
 	return (str);
 }
