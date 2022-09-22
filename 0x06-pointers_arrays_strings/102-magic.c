e8cefa221276a475111ea221acf368dda1d6d757
bekiyeromm <bk3tena@gmail.com>
2022-09-22 12:50:11 -0700

    dd one line to this code, so that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -21,10 +21,8 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	;
-	/* ...so that this prints 98\n */
 	*(p + 5) = 98;
-	/* prints 98 followed by new line */
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
