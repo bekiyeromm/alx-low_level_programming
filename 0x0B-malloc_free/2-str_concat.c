527f92ab9098acec9f780b4ad700e83f21d55b6a
bekiyeromm <bk3tena@gmail.com>
2022-10-03 23:11:07 -0700

    Write a function that concatenates two strings.The returned pointer should point to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated if null passed treat as empty string return null on faillure

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * str_concat -concatenate string s1 and s2
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: return concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1 = strlen(s1), len2 = strlen(s2);
+	char *concat_string;
+
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
+
+	concat_string = malloc(sizeof(char) * (len1 + len2));
+	for (i = 0; s1[i]; i++)
+		concat_string[i] = s1[i];
+	for (i = 0; s2[i]; i++)
+		concat_string[i + len1] = s2[i];
+	return (concat_string);
+}
