78c15bd2b1b2028fb3dfc3395aa1ce883d177943
bekiyeromm <bk3tena@gmail.com>
2022-10-10 14:52:03 -0700

    Write a function that prints a struct dog Prototype: void print_dog(struct dog *d) If an element of d is NULL, print (nil) instead of this element. (if name is NULL, print Name: (nil))

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * print_dog -prints dogs detail
+ * @d: dog tag
+ * Description: prints details of dog
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil) \n");
+	else if (d->name == NULL || &(*d).age == NULL || d->owner == NULL)
+		printf("(nil) \n");
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
