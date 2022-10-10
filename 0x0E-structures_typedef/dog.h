4b7eb388c7b54be7eeb6c6d4cda888c2f3d3cef5
bekiyeromm <bk3tena@gmail.com>
2022-10-10 14:12:29 -0700

    dog.h file modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog -structure tag name
  * Description: creates struct variable
