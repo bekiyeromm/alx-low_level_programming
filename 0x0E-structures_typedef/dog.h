26236552882fe9e426a2f94efb3651aedbf81af3
bekiyeromm <bk3tena@gmail.com>
2022-10-10 02:47:40 -0700

    dog.h file  modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,6 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
 struct dog
 {
