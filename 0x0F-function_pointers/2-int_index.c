901bd311b5bf85cb677a795d6c4ff30cb49e90ee
bekiyeromm <bk3tena@gmail.com>
2022-10-12 11:14:57 -0700

    Write a function that searches for an integer.int_index returns the index of the first element for which the cmp function does not return 0 If no element matches, return -1 If size <= 0, return -1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+/**
+ * int_index -prints idex to which matches
+ * found
+ * @array: array
+ *
+ * @size: size of the array
+ *
+ * @cmp: pointer to the function
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		i = 0;
+		while (i < size)
+		{
+			if ((*cmp)(array[i]))
+				return (i);
+			i++;
+		}
+	}
+	return (-1);
+}
