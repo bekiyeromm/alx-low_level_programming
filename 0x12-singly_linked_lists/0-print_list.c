a89e9e28b95a6d4f386cdc2224726adc98a38862
bekiyeromm <bk3tena@gmail.com>
2022-10-20 12:30:19 -0700

    Write a function that prints all the elements of a list_t list If str is NULL, print [0] (nil) return the number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list -prints elements of linkedlist
+ * @h: linked list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+	if (h -> str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	c += 1;
+	}
+	return (c);
+}
