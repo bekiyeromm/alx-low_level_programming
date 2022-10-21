fcd638a80939a2f7ce27c575e6ed75edf37a9434
bekiyeromm <bk3tena@gmail.com>
2022-10-21 08:53:22 -0700

    Write a function that prints You're beat! and yet, you must allow, bore my house upon my back! before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void fun(void) __attribute__ ((constructor));
+
+/**
+ *entryFunc - function to print before main function
+ *
+ *Return: Void
+ */
+
+void fun(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
