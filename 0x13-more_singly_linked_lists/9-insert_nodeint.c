fb26f27e87cb8d85aabe0735ddee518e03e54828
bekiyeromm <bk3tena@gmail.com>
2022-10-24 23:44:39 -0700

    Write a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->n = n;
 	ptr2->next = NULL;
 
-	while (i < idx - 1)
+	while (i != idx - 1)
 	{
 		ptr = ptr->next;
 		i++;
