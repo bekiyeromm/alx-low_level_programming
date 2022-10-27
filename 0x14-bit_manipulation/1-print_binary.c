22e3a12d1cfb10e451e359e90c46f0febc7e610f
bekiyeromm <bk3tena@gmail.com>
2022-10-27 13:43:14 -0700

    print binnary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned int j, k, mask;
-
-	for (i = 32; i >= 0; i--)
-	{
-		j = i;
-		mask = 1 << j;
-		k = n & mask;
-		k == 0 ? _putchar('0') : _putchar('1');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
