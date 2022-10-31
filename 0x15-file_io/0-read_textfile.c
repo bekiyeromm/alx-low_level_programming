e97c12e6186ac89ce3bd86b902867a9518321486
bekiyeromm <bk3tena@gmail.com>
2022-10-31 16:30:27 -0700

    Write a function that reads a text file and prints it to the POSIX standard output where letters is the number of letters it should read and print returns the actual number of letters it could read and print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,12 +10,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file1, file2;
 	size_t readed;
 	char *buffer = malloc(sizeof(char) * letters);
-	
+
 	if (buffer == NULL)
 		return (0);
 	if (filename == NULL)
 		return (0);
-	
+
 	file1 = open(filename, O_RDONLY, 0600);
 	if (file1 == -1)
 	{
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	readed = read(file1, buffer, letters);
 	buffer[letters] = '\0';
 	close(file1);
-	
+
 	file2 = write(STDOUT_FILENO, buffer, readed);
 	if (file2 == -1)
 		return (0);
