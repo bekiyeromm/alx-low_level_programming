4d1c4ee9bf534c1b35280c225757142f98813156
bekiyeromm <bk3tena@gmail.com>
2022-10-31 18:28:23 -0700

    main.h file updated:

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _copy(const char *src, const char *dest);
+int main(int argc, char **argv);
 
 #endif
