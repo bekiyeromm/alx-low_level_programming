1286826479ca4556418b41dcc5fffe5557ae9a14
bekiyeromm <bk3tena@gmail.com>
2022-12-08 09:30:43 -0800

    Write a function that inserts a new node at a given position.
    Prototype: dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
    where idx is the index of the list where the new node should be added. Index starts at 0
    Returns: the address of the new node, or NULL if it failed
    if it is not possible to add the new node at index idx, do not add the new node and return NULL
    Your files 2-add_dnodeint.c and 3-add_dnodeint_end.c will be compiled during the correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 /**
  * insert_dnodeint_at_index - inserts anode at given index
  * @h: pointer to head
- * @idx: index to which value will be inserted 
+ * @idx: index to which value will be inserted
  * @n: value to be inserted at index idx
  *
  * Return: the address of the new node, or NULL if it failed
@@ -13,12 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp;
 
 	if (h == NULL)
-		return (0);
+		return (new_node);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
+
 	if (idx == 0)
 	{
 		new_node->next = (*h);
@@ -27,7 +29,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h) = new_node;
 		return (new_node);
 	}
-	new_node->n = n;
 	temp = *h;
 
 	while (temp)
@@ -43,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (new_node);
 		}
 		temp = temp->next;
-	 }
+	}
 	free(new_node);
 	return (NULL);
 }
