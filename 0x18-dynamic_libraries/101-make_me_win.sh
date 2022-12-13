ff240cff0acdb494f38e7c2902cbb5868e7a7687
bekiyeromm <bk3tena@gmail.com>
2022-12-13 03:04:31 -0800

    101- make me win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -p /tmp/https://raw.github.com/bekiyeromm/alx-low_level_programming/
-master/0x18-dynamic_libraries/file1.so
-export LD_PRELOAD =/tmp/file1.so
+wget -p /tmp/https://raw.github.com/bekiyeromm/alx-low_level_programming/tree/master/0x18-dynamic_libraries/file1.so
+export LD_PRELOAD=/tmp/file1.so
