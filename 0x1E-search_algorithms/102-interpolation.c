36c5b8e98ccbde94e5addee333bd23a935e45e3d
berekettena <tena21bereket@gmail.com>
2023-04-12 20:26:44 +0300

    Write a function that searches for a value in a sorted array of integers using the Interpolation search algorithm
    Prototype : int interpolation_search(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the first index where value is located
    You can assume that array will be sorted in ascending order
    If value is not present in array or if array is NULL, your function must return -1
    To determine the probe position, you can use : size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]))
    Every time you compare a value in the array to the value you are searching, you have to print this value (see example below)

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,34 +9,27 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-	{
-		return (-1);
-	}
 	size_t pos = 0, low = 0, high = size - 1;
+
+	if (!array || !size)
+		return (-1);
 
 	while (1)
 	{
-		pos = low + (((double)(high - low) / (
-			array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) /
+			  (array[high] - array[low])) * (value - array[low]));
 		if (pos >= size)
 		{
 			printf("Value checked array[%lu] is out of range\n", pos);
 			break;
 		}
-	printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
-		{
 			return (pos);
-		}
-		if (array[pos] < value)
-		{
-			low = pos + 1;
-		}
-		else
-		{
+		else if (array[pos] > value)
 			high = pos - 1;
-		}
+		else
+			low = pos + 1;
 	}
-return (-1);
+	return (-1);
 }
