c96c41a0377383127da7e4909f41fd6eb548176d
berekettena <tena21bereket@gmail.com>
2023-04-19 14:20:45 +0300

    modified

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -22,6 +22,7 @@ typedef struct listint_s
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+int binary_search_2(int *array, int left, int right, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
