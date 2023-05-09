4889977b75e74ec98d5b1a2f66c8a4385d2d60b5
berekettena <tena21bereket@gmail.com>
2023-05-09 11:53:11 +0300

    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm
    Prototype : int binary_search(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the index where value is located
    You can assume that array will be sorted in ascending order
    You can assume that value won’t appear more than once in array
    If value is not present in array or if array is NULL, your function must return -1
    You must print the array being searched every time it changes. (e.g. at the beginning and when you search a subarray) (See example)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,28 +1,30 @@
 #include "search_algos.h"
 /**
- * binary_search -searches for a value in an array of integers-
- * using the binary search algorithm
- * @array:pointer to the first element of the array to search in
- * @size:is the number of elements in array
- * @value: is the value to search for
- * Return:the first index where value is located or -1
+ * binary_search - search a value in a sorted array of integers using Binary
+ * @array: pointer to the first element of the array to search
+ * @size: is the number of elements in array
+ * @value: is the number of elements in array
+ * Return: the index where value is located or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left = 0;
+	size_t right = size - 1;
 	size_t i;
-	size_t left = 0, right = size - 1, mid;
+/*	int *a = array;*/
 
-	if (!array || size == 0)
+	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
 	while (left <= right)
 	{
-		mid = (left + right) / 2;
+		int mid = left + (right - left) / 2;
+
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d", array[i]);
 			if (i < right)
 			{
 				printf(", ");
